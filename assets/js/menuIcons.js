d714e38bb0440eca7b69b1b4ee2c8b19b3d3e0b7
Ana Aldeguer <anaaldfer@gmail.com>
2025-01-28 16:51:45 +0100

    menu responsive

--- assets/js/menuIcons.js
+++ assets/js/menuIcons.js
@@ -1,9 +1,9 @@
 // JS for responsive menu - icon toggle
 
 const checkbtnContainer = document.querySelector(".checkbtn");
-const checkbtn = document.querySelector("checkbtn i");
+const checkbtn = document.querySelector(".checkbtn i");
 
-void checkbtnContainer.addEventListener("click", function () {
+checkbtnContainer.addEventListener("click", function () {
   checkbtn.classList.toggle("fa-solid fa-xmark");
   checkbtn.classList.toggle("fa-solid fa-bars");
 });
